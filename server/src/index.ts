6c259be9770f981c722b32b27efe4b7561a69896
Yevhenii Solovei <146028527+TheDidEva@users.noreply.github.com>
2024-03-26 12:23:17 +0200

    u

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,20 +38,9 @@ const main = async () => {
         preflightContinue: false,
         optionsSuccessStatus: 204
     }
-    app.use(cors(corsOptions));
-    app.use(cookieParser())
-    app.use(express.json())
-    app.use(function (req: Request, res: Response, next: NextFunction) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Methods",
-            "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-        );
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-    });
+    app.use(cors());
+    app.use(cookieParser());
+    app.use(express.json());
 
     app.use('/user', userRouter);
     app.use('/collectionWords', collectionWordsRouter);
