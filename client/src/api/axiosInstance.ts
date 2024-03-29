8477f2eb3da9e94e06d9d82bdedf5f108c93c561
Yevhenii Solovei <146028527+TheDidEva@users.noreply.github.com>
2024-03-29 19:43:16 +0200

    u

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 //http://localhost:5000
 //https://verbumlearnserv.onrender.com
 
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://verbumlearnserv.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
